706e89659a27277459b95f53fe510dd2dac2b6db
Jeanette <aejlindberg@gmail.com>
2018-10-04 13:04:46 +0200

    map and if

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,32 +1,42 @@
 import React from "react"
-// import Station from "./station.js"
+import Station from "./station.js"
 
 class App extends React.Component {
 
-constructor(props) {
-super(props)
-this.state = {
-radioItems: []
-}
-}
-componentDidMount() {
-    fetch("http://api.sr.se/api/v2/channels?format=json&size=100").then((response) => {
-      return response.json()
-    }).then((json) => {
-      this.setState({
-        radioItems: json.channels
+  constructor(props) {
+    super(props)
+    this.state = {
+      radioItems: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://api.sr.se/api/v2/channels?format=json&size=100")
+      .then((response) => {
+        return response.json()
+      }).then((json) => {
+        this.setState({
+          radioItems: json.channels
+        })
       })
-    })
-}
+  }
 
   render() {
     if (this.state.radioItems.length > 0) {
-      return <div>{this.state.radioItems[1].name}</div>
+      // return <div>{this.state.radioItems[1].name}</div>
+      return (
+        this.state.radioItems.map((station) => {
+          return <Station
+            name={station.name}
+            image={station.image}
+            tagline={station.tagline} />
+        })
+
+      )
     } else {
       return <div>Loading...</div>
     }
   }
-
 }
 
 export default App
--- code/src/components/station.js
+++ code/src/components/station.js
@@ -5,9 +5,9 @@ class Station extends React.Component {
   render() {
     return (
 
-      <div className="station"
+      <div className="station">
         <h1>{this.props.name}</h1>
-        <img src={this.props.image} alt="station"/>
+        <img src={this.props.image} alt="station" />
         <p>{this.props.tagline}</p>
       </div>
 
