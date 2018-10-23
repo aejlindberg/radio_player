e502fee1090012578d9ae16d368139f633f85e6d
Jeanette <aejlindberg@gmail.com>
2018-10-23 15:31:34 +0200

    Made background img responsive

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -25,24 +25,26 @@ class App extends React.Component {
     if (this.state.radioItems.length > 0) {
       return (
         <div className="App">
-          <div className="backgroundImage">
-            <img src="/background.jpg" alt="confetti" />
+          <div className="wrapper">
+            <div className="backgroundImage">
+              <img src="/background.jpg" alt="confetti" />
+            </div>
+            <div className="header">
+              <img id="SR" src="./SR.svg" alt="" />
+              <img id="radio" src="./radio.svg" alt="" />
+            </div>
+            {this.state.radioItems.map((station) => {
+              return (
+                <Station
+                  key={station.id}
+                  name={station.name}
+                  image={station.image}
+                  tagline={station.tagline}
+                  color={station.color}
+                  liveaudioUrl={station.liveaudio.url} />
+              )
+            })}
           </div>
-          <div className="header">
-            <img id="SR" src="./SR.svg" alt="" />
-            <img id="radio" src="./radio.svg" alt="" />
-          </div>
-          {this.state.radioItems.map((station) => {
-            return (
-              <Station
-                key={station.id}
-                name={station.name}
-                image={station.image}
-                tagline={station.tagline}
-                color={station.color}
-                liveaudioUrl={station.liveaudio.url} />
-            )
-          })}
         </div>
       )
     } else {
